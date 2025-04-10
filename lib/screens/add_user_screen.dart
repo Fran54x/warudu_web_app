a4dd60d664293d1ac93ecb8c11acb97a4132110c
Fran54x <riostorres2@gmail.com>
2025-04-09 22:51:12 -0600

    Issue of type user fixed

--- lib/screens/add_user_screen.dart
+++ lib/screens/add_user_screen.dart
@@ -56,7 +56,7 @@ class _AddUserScreenState extends State<AddUserScreen> {
       'imagen': _imagenController.text,
       'correo': _correoController.text,
       'password': _passwordController.text,
-      'tipo_usuario': _selectedOption,
+      if (_selectedOption != 0) 'tipo_usuario': _selectedOption,
     });
 
     // Validar correo electrónico
