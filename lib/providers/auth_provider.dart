f4a1a1fbf996e6f4ef3094478cb33a9443084202
Fran54x <riostorres2@gmail.com>
2025-05-09 16:13:03 -0600

    Update auth provider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider with ChangeNotifier {
   bool _isAuthenticated = false;
 
   bool get isAuthenticated => _isAuthenticated;
 
-  void login() {
+  AuthProvider() {
+    _loadAuthStatus();
+  }
+
+  Future<void> _loadAuthStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isAuthenticated = prefs.getBool('isAuthenticated') ?? false;
+    notifyListeners(); // Notifica después de cargar el valor
+  }
+
+  void login() async {
     _isAuthenticated = true;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isAuthenticated', true);
     notifyListeners();
   }
 
-  void logout() {
+  void logout() async {
     _isAuthenticated = false;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isAuthenticated', false);
     notifyListeners();
   }
 }
